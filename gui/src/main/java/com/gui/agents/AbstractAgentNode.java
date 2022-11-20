8f05076a96e2b80ca0fb63548d1b36fcbd3c9349
soffi49 <zosia.wrona49@gmail.com>
2022-11-20 13:50:51 +0100

    [GC-46] Fixed merging bug

--- gui/src/main/java/com/gui/agents/AbstractAgentNode.java
+++ gui/src/main/java/com/gui/agents/AbstractAgentNode.java
@@ -30,9 +30,6 @@ public abstract class AbstractAgentNode implements AbstractAgentNodeInterface {
 		this.agentName = agentName;
 	}
 
-	protected AbstractAgentNode() {
-	}
-
 	@Override
 	public String getAgentName() {
 		return agentName;
