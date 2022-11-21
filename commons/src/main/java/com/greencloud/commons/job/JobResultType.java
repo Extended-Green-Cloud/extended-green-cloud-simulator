77e935163789585ccf0da5c0a83659338d4b7356
soffi49 <zosia.wrona49@gmail.com>
2022-11-21 06:00:09 +0100

    [GC] Removed PROCESS status and added FAILED

--- commons/src/main/java/com/greencloud/commons/job/JobResultType.java
+++ commons/src/main/java/com/greencloud/commons/job/JobResultType.java
@@ -4,5 +4,5 @@ package com.greencloud.commons.job;
  * Enum containing possible outcomes of the job execution
  */
 public enum JobResultType {
-	PROCESSED, ACCEPTED, FINISH, STARTED
+	FAILED, ACCEPTED, FINISH, STARTED
 }
